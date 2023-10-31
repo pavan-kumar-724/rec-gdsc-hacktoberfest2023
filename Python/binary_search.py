1b81c4d5f6fec1c3c29e5ecb7e4fe10fc880d668
N Pavan Kumar <130792413+pavan-kumar-724@users.noreply.github.com>
2023-10-31 23:08:43 +0530

    Update binary_search.py
    Added documentation to the code

--- Python/binary_search.py
+++ Python/binary_search.py
@@ -1,25 +1,26 @@
-
+# binary search using recursion
+# array is assumed to be sorted
 def binary_search(arr, low, high, x):
 
 	if high >= low:
 
-		mid = (high + low)//2
-		if arr[mid] == x:
+		mid = (high + low)//2  #finding the middle element using floor division
+		if arr[mid] == x: #if the element to be searched is the middle element
 			return mid
 		    
-		elif arr[mid] > x:
+		elif arr[mid] > x: #if less than middle element, search the left sub-array 
 			return binary_search(arr, low, mid - 1, x)
 
-		else:
+		else: #if greater than middle element, search the right sub-array
 			return binary_search(arr, mid + 1, high, x)
 
-	else:
+	else: #not found
 		return -1
 
 arr = [ 2, 3, 4, 10, 40 ]
 x = 10
 
-result = binary_search(arr, 0, len(arr)-1, x)
+result = binary_search(arr, 0, len(arr)-1, x) #function call
 
 if result != -1:
 	print("Element is present at index", str(result))
